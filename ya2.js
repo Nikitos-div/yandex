858cf165ff7865622b3a8f07124d812c9e50ceec
Никита <n.churakov@takewing.ru>
2020-02-29 20:18:46 +0300

    good func

--- ya2.js
+++ ya2.js
@@ -1,18 +1,16 @@
-const TEST = 'aaaaaaabbbbbbbcccccc'
+const TEST = 'aaaaaaabbbbdddcccccc'
 const f = (str, ideal) => {
 	let cache = ideal[0]
 	let arrStr = [ideal[0]]
 	for (let i = 1; i < ideal.length; ++i) {
 		if (ideal[i] != cache) {
 			cache = ideal[i]
-			//		console.log(cache)
 			arrStr.push(cache)
 		}
 		else {
 			continue;
 		}
 	}
-	console.log(arrStr.join(''))
 	return arrStr.join('') === str ? true : false
 }
 
